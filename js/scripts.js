1088830433b44e239651c47f3c59320cfb8072c1
OscarVasquez1 <va17011@ues.edu.sv>
2022-10-21 21:03:46 -0600

    Guia_61c-API REST v.2.0
    Version 2 de la guia 61c de Programación FrontEnd y Creación de API-REST de pruebas.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+(function($) {
+    "use strict"; 
+	
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+ 
+
+})(jQuery);
